a52fd2d4600463cc633d5f89dbbee5a1ac7713d6
Chaeruin <cms3604@naver.com>
2025-10-31 13:55:05 +0900

    fix: 요구사항에 맞는 에러 메세지 생성

--- src/main/java/lotto/exception/ParseErrorCode.java
+++ src/main/java/lotto/exception/ParseErrorCode.java
@@ -3,14 +3,14 @@ package lotto.exception;
 public enum ParseErrorCode {
 
     // 당첨 번호 입력 파싱 에러 예외 처리
-    WINNING_NUMBER_PARSING_ERROR("구분자가 올바르지 않습니다."),
-    WINNING_NUMBER_IS_NOT_INT("숫자가 정수가 아닙니다."),
+    WINNING_NUMBER_PARSING_ERROR("[ERROR] 구분자가 올바르지 않습니다."),
+    WINNING_NUMBER_IS_NOT_INT("[ERROR] 숫자가 정수가 아닙니다."),
 
     // 구입 금액 파싱 에러 예외 처리
-    PURCHASE_AMOUNT_IS_NOT_INT("구입 금액이 정수가 아닙니다."),
+    PURCHASE_AMOUNT_IS_NOT_INT("[ERROR] 구입 금액이 정수가 아닙니다."),
 
     // 보너스 번호 파싱 에러 예외 처리
-    BONUS_NUMBER_IS_NOT_INT("보너스 번호가 정수가 아닙니다."),;
+    BONUS_NUMBER_IS_NOT_INT("[ERROR] 보너스 번호가 정수가 아닙니다."),;
 
 
     private final String errorMessage;
