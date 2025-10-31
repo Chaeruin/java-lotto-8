0bc653bb3d98a3c35db79e961811ddcdb4422d9e
Chaeruin <cms3604@naver.com>
2025-10-31 15:34:08 +0900

    feat: Lotto 클래스 내부 도메인 자체 검증 추가 구현 및 getter 구현 시 외부에 컬렉션 직접 노출 방지하도록 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.exception.ValidateErrorCode;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,11 +11,35 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return List.copyOf(numbers); // 외부에 내부 컬렉션 노출 방지
+    }
+
     private void validate(List<Integer> numbers) {
+        validateNumbersSize(numbers);
+        validateNumbersInRange(numbers);
+        validateNumbersDuplicated(numbers);
+    }
+
+    private void validateNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ValidateErrorCode.LOTTO_INTEGER_LIST_SIZE_IS_NOT_VALID.getErrorMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateNumbersInRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(num -> !isInRange(num))) {
+            throw new IllegalArgumentException(ValidateErrorCode.LOTTO_NUMBER_IS_NOT_IN_RANGE.getErrorMessage());
+        }
+    }
+
+    private void validateNumbersDuplicated(List<Integer> numbers) {
+        if (numbers.stream().distinct().toList().size() != numbers.size()) {
+            throw new IllegalArgumentException(ValidateErrorCode.LOTTO_INTEGER_LIST_IS_DUPLICATED.getErrorMessage());
+        }
+    }
+
+    private boolean isInRange(int num) {
+        return num >= 1 && num <= 45;
+    }
 }
