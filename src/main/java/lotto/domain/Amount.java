551d763b2dbc89cb0f1a955b427b3c0e343113cb
Chaeruin <cms3604@naver.com>
2025-10-31 16:51:22 +0900

    feat: Amount Entity 구현 및 내부 검증 로직 구현, dto to Entity from 정적 메서드 구현

---
+++ src/main/java/lotto/domain/Amount.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import lotto.dto.AmountDto;
+import lotto.exception.ValidateErrorCode;
+
+public class Amount {
+
+    private final int amount;
+
+    public Amount(int amount) {
+        validate(amount);
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public static Amount from(AmountDto amountDto) {
+        return new Amount(amountDto.amount());
+    }
+
+    private void validate(int amount) {
+        validateIsInRange(amount);
+        validateCanDivided(amount);
+    }
+
+    private void validateIsInRange(int amount) {
+        if (amount > 100_000) {
+            throw new IllegalArgumentException(ValidateErrorCode.PURCHASE_AMOUNT_IS_OVER_RANGE.getErrorMessage());
+        }
+    }
+
+    private void validateCanDivided(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException(ValidateErrorCode.PURCHASE_AMOUNT_IS_NOT_DIVIDED.getErrorMessage());
+        }
+    }
+}
