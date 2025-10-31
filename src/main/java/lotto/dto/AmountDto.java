36082ff149932d58cb566e98e7b2eaa1341801d5
Chaeruin <cms3604@naver.com>
2025-10-31 13:25:42 +0900

    feat: 필요 Dto 구현

---
+++ src/main/java/lotto/dto/AmountDto.java
@@ -0,0 +1,6 @@
+package lotto.dto;
+
+public record AmountDto(
+        int amount
+) {
+}
---
+++ src/main/java/lotto/dto/WinningNumberDto.java
@@ -0,0 +1,8 @@
+package lotto.dto;
+
+import java.util.List;
+
+public record WinningNumberDto(
+        List<Integer> winningNumbers
+) {
+}
