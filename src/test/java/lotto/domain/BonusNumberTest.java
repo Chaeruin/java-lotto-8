e8832fdc6e06873a486923a35d27ecb401cd2875
Chaeruin <cms3604@naver.com>
2025-11-02 16:35:30 +0900

    test: 매직넘버 치환

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import lotto.enums.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,14 +11,14 @@ class BonusNumberTest {
     @Test
     @DisplayName("1 이상 45 이하가 아니면 예외_1 미만")
     void test1() {
-        assertThatThrownBy(() -> new BonusNumber(0))
+        assertThatThrownBy(() -> new BonusNumber(LottoNumber.MIN_LOTTO_NUMBER.getNumber() - 1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("1 이상 45 이하가 아니면 예외_45 초과")
     void test2() {
-        assertThatThrownBy(() -> new BonusNumber(46))
+        assertThatThrownBy(() -> new BonusNumber(LottoNumber.MAX_LOTTO_NUMBER.getNumber() + 1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
