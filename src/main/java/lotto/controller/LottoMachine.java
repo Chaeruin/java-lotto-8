62e5e2f5e181c132f79fc7faf0fffb2cbef122d0
Chaeruin <cms3604@naver.com>
2025-11-02 15:01:36 +0900

    feat: 출력 로직 제외 메인 로직 구현

--- src/main/java/lotto/controller/LottoMachine.java
+++ src/main/java/lotto/controller/LottoMachine.java
@@ -29,6 +29,22 @@ public class LottoMachine {
     }
 
     public void run() {
+        // 구입 금액 입력
+        Amount amount = Amount.from(InputParser.parseAmount(InputView.getAmount()));
+        // 구입 금액에 따른 로또 발행
+        List<Lotto> lottery = issue.issueLottery(amount);
+        // 발행 로또 리스트 출력
 
+        // 당첨 번호 입력
+        Lotto winningNumber = Lotto.from(InputParser.parseWinningNumbers(InputView.getWinningNumbers()));
+        // 보너스 번호 입력
+        BonusNumber bonusNumber = BonusNumber.from(InputParser.parseBonusNumber(InputView.getBonusNumber()));
+
+        // 로직 !@#$!@#$
+        TotalPrize totalPrize = winningLottery.getTotalWinnings(lottery, winningNumber, bonusNumber);
+        // 당첨 통계 포멧애 맞게 출력
+
+        // 수익률 출력
+        double returnRating = returnRate.getReturnRate(amount, totalPrize.calculateTotalPrizeAmount());
     }
 }
