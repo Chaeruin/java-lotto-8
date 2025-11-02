5fe66008d6f30657c51311246517ed77b825e785
Chaeruin <cms3604@naver.com>
2025-11-02 15:17:45 +0900

    feat: closeConsole 구현

--- src/main/java/lotto/controller/LottoMachine.java
+++ src/main/java/lotto/controller/LottoMachine.java
@@ -32,26 +32,30 @@ public class LottoMachine {
     }
 
     public void run() {
-        // 구입 금액 입력
-        Amount amount = Amount.from(InputParser.parseAmount(InputView.getAmount()));
-        // 구입 금액에 따른 로또 발행
-        List<Lotto> lottery = issue.issueLottery(amount);
-        // 발행 로또 리스트 출력
-        List<LottoResponseDto> lottoResponseDto = getLottoResponseDto(lottery);
+        try {
+            // 구입 금액 입력
+            Amount amount = Amount.from(InputParser.parseAmount(InputView.getAmount()));
+            // 구입 금액에 따른 로또 발행
+            List<Lotto> lottery = issue.issueLottery(amount);
+            // 발행 로또 리스트 출력
+            List<LottoResponseDto> lottoResponseDto = getLottoResponseDto(lottery);
 
 
-        // 당첨 번호 입력
-        Lotto winningNumber = Lotto.from(InputParser.parseWinningNumbers(InputView.getWinningNumbers()));
-        // 보너스 번호 입력
-        BonusNumber bonusNumber = BonusNumber.from(InputParser.parseBonusNumber(InputView.getBonusNumber()));
+            // 당첨 번호 입력
+            Lotto winningNumber = Lotto.from(InputParser.parseWinningNumbers(InputView.getWinningNumbers()));
+            // 보너스 번호 입력
+            BonusNumber bonusNumber = BonusNumber.from(InputParser.parseBonusNumber(InputView.getBonusNumber()));
 
-        // 로직 !@#$!@#$
-        TotalPrize totalPrize = winningLottery.getTotalWinnings(lottery, winningNumber, bonusNumber);
-        // 당첨 통계 포멧애 맞게 출력
-        TotalPrizeResponseDto totalPrizeResponseDto = TotalPrizeResponseDto.of(totalPrize);
+            // 로직 !@#$!@#$
+            TotalPrize totalPrize = winningLottery.getTotalWinnings(lottery, winningNumber, bonusNumber);
+            // 당첨 통계 포멧애 맞게 출력
+            TotalPrizeResponseDto totalPrizeResponseDto = TotalPrizeResponseDto.of(totalPrize);
 
-        // 수익률 출력
-        double returnRating = returnRate.getReturnRate(amount, totalPrize.calculateTotalPrizeAmount());
+            // 수익률 출력
+            double returnRating = returnRate.getReturnRate(amount, totalPrize.calculateTotalPrizeAmount());
+        } finally {
+            InputView.closeConsole();
+        }
     }
 
     private List<LottoResponseDto> getLottoResponseDto(List<Lotto> lottery) {
