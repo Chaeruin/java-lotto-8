124d70e21331890480c12d4929914c8b8cc55c79
Chaeruin <cms3604@naver.com>
2025-11-02 12:45:04 +0900

    feat: TotalPrize - 모든 당첨 항목에 대한 당첨 내역 추가 및 총 당첨금 계산

---
+++ src/main/java/lotto/domain/TotalPrize.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.EnumMap;
+import java.util.Map;
+import lotto.enums.Prize;
+
+public class TotalPrize {
+
+    private final Map<Prize, Integer> winnings;
+
+    public TotalPrize() {
+        winnings = new EnumMap<>(Prize.class);
+    }
+
+    public void addPrize(Prize prize, int count) {
+        winnings.put(prize, winnings.getOrDefault(prize, 0) + count);
+    }
+
+    public int calculateTotalPrizeAmount() {
+        return winnings.entrySet().stream()
+                .mapToInt(entry ->
+                        entry.getKey().getPrizeMoney() * entry.getValue())
+                .sum();
+    }
+}
